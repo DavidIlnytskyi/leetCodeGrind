f7d29ae76a6d588df1e588d647fcf9e0fed9a0c7
David <ilnytskyi.pn@ucu.edu.ua>
2024-01-02 19:23:41 +0200

    Add better solution and explanation

--- DailyChallange/ConvertAnArray.cpp
+++ DailyChallange/ConvertAnArray.cpp
@@ -12,37 +12,60 @@ public:
     vector<vector<int>> static findMatrix(vector<int>& nums) {
         int maxValue = 0;
         vector<int> uniqueNumbers;
-        map<int, int> numbers;
+        map<int, int> numbersQuantity;
 
         for(int number : nums){
-            if (numbers[number] == 0){
+            if (numbersQuantity[number] == 0){
                 uniqueNumbers.push_back(number);
             }
-            ++numbers[number];
-            maxValue = max(numbers[number], maxValue);
+            ++numbersQuantity[number];
+            maxValue = max(numbersQuantity[number], maxValue);
         }
         vector<vector<int>> result(maxValue);
 
         for(int idx = 0; idx < maxValue; idx++){
             for(int number : uniqueNumbers){
-                if(numbers[number] != 0){
-                    --numbers[number];
+                if(numbersQuantity[number] != 0){
+                    --numbersQuantity[number];
                     result[idx].push_back(number);
                 }
             }
         }
 
+        return result;
+    }
+};
+
+/*
+Main idea:
+
+Ми створюємо додатковий масив, що буде
+містити кількість входженнь кожного числа.
+Кожного разу, як ми зустрічаємо число ми перевіряємо
+кількість входжень на попередньому кроці. Якщо ця кількість
+вже є максимальною(тобто входжень є стільки скільки і рядків),
+то ми створюємо новий рядок для нового входження і додаємо його туди,
+збільшуючи кількість входжень на один. Якщо кількість входжень на
+попередньому кроці є менша, ніж кількість рядків, то ми просто
+додаємо число до існуючого рядка і збільшуєм кількість входжень на 1.
+
+class Solution {
+public:
+    vector<vector<int>> static findMatrix(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> frequency(nums.size() + 1);
+
+    for(int number : nums){
+        if(frequency[number] == result.size()){
+            result.push_back({});
+        }
+
+        result[frequency[number]].push_back(number);
+        frequency[number]++;
+    }
+
     return result;
     }
 };
 
-int main(){
-    vector<int> input = {1,3,4,1,2,3,1};
-    vector<vector<int>> result = Solution().findMatrix(input);
-    for(int vectorNum = 0; vectorNum < result.size(); ++vectorNum){
-        for(int insVecSize = 0; insVecSize < result[vectorNum].size(); ++insVecSize){
-            cout << result[vectorNum][insVecSize] << " ";
-        }
-        cout << endl;
-    }
-}
+ */
