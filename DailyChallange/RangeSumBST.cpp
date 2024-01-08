bedebbe915cd9f33efa93307504ca65d7bd49d21
DavidIlnytskyi <ilnytskyi.pn@ucu.edu>
2024-01-08 23:51:04 +0200

    Add RangeSumBST

---
+++ DailyChallange/RangeSumBST.cpp
@@ -0,0 +1,21 @@
+/*
+ https://leetcode.com/problems/range-sum-of-bst/?envType=daily-question&envId=2024-01-08
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+
+    }
+};
