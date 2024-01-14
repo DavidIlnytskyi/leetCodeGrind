6e7ae087b528ee1687c7f682e604574dfec9d93a
DavidIlnytskyi <ilnytskyi.pn@ucu.edu.ua>
2024-01-14 14:03:46 +0200

    Change BFS to DFS

--- 75/KeysAndRooms.cpp
+++ 75/KeysAndRooms.cpp
@@ -11,7 +11,7 @@ public:
 
         int numberOfVisitedRooms = 0;
         map<int, int> visitedRooms;
-        queue<int> keys;
+        stack<int> keys;
 
         keys.push(0);
         visitedRooms[0] = 1;
@@ -19,7 +19,10 @@ public:
         while(!keys.empty()){
             numberOfVisitedRooms++;
 
-            for(int roomKey : rooms[keys.front()]){
+            int topKey = keys.top();
+            keys.pop();
+
+            for(int roomKey : rooms[topKey]){
                 if(visitedRooms[roomKey] != 0)
                     continue;
 
@@ -27,7 +30,6 @@ public:
                 keys.push(roomKey);
             }
 
-            keys.pop();
         }
 
         return numberOfVisitedRooms == rooms.size();
@@ -38,12 +40,12 @@ public:
 /*
  Main idea:
 
- Створюєм чергу, по якій будемо ходити по кімнатах.
+ Створюєм чергу(стак), по якій будемо ходити по кімнатах.
  Спочатку додаємо туди 0 кімнату, адже ми починаємо
  завдання з неї. Далі для кожного ключа в кімнаті
  перевіряємо, чи ми вже маємо цей ключ. Якщо він
- в нас є, не додаємо його в чергу. Якщо його немає,
- додаємо його в чергу й залишаєм позначку, що ми
+ в нас є, не додаємо його в стак. Якщо його немає,
+ додаємо його в черга й залишаєм позначку, що ми
  такий ключ вже маємо. Поки в нас не закінчаться ключі,
  заходимо в кімнату, беремо всі ключі і йдемо в наступну
  по черзі.
